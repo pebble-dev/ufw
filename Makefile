37fe5fca20895c6896ba87b11f727243b3886493
Joshua Wise <joshua@joshuawise.com>
2016-12-29 16:50:35 -0800

    use CMSIS defines

--- Makefile
+++ Makefile
@@ -1,14 +1,26 @@
 PEBBLE_TOOLCHAIN_PATH ?= /usr/local/Cellar/pebble-toolchain/2.0/arm-cs-tools/bin
 
+ifneq "$(WAIT_GDB)" ""
+QEMUFLAGS += -S
+endif
+
 PFX ?= $(PEBBLE_TOOLCHAIN_PATH)/arm-none-eabi-
 CC = $(PFX)gcc
 
-OBJS = boot.o
+ASFLAGS += -ggdb
+CFLAGS += -g
+
+CFLAGS += -mcpu=cortex-m3 -mthumb -Icmsis
+
+OBJS = boot.o main.o
 
 target: fw.qemu_flash.bin
 
 qemu: fw.qemu_flash.bin
-	qemu-pebble -rtc base=localtime -serial null -serial null -serial stdio -gdb tcp::63770,server -machine pebble-bb2 -cpu cortex-m3 -pflash fw.qemu_flash.bin
+	qemu-pebble -rtc base=localtime -serial null -serial null -serial stdio -gdb tcp::63770,server -machine pebble-bb2 -cpu cortex-m3 -pflash fw.qemu_flash.bin $(QEMUFLAGS)
+
+gdb:
+	$(PFX)gdb -ex 'target remote localhost:63770' -ex 'sym fw.elf'
 
 fw.elf: $(OBJS) fw.lds
 	$(PFX)ld -o $@ -T fw.lds $(OBJS) 
--- main.c
+++ main.c
@@ -1,19 +1,9 @@
-#define UART_SR_TXE (1 << 7)
-#define UART_SR (0x00)
-#define UART_DR (0x04)
-#define UART1 0x40011000
-#define UART2 0x40004400
-#define UART3 0x40004800
-#define UARTDBG UART3
-
-#define RCC 0x40023800
-#define RCC_APB1ENR (RCC + 0x40)
-#define RCC_APB1ENR_UART3 (1 << 18)
+#include <stm32f2xx.h>
 
 int putchar(int c) {
-	while (!(*(unsigned char *)(UARTDBG + UART_SR) & UART_SR_TXE))
+	while (!(USART3->SR & USART_SR_TXE))
 		; /* we are efficient, here! */
-	*(unsigned char *)(UARTDBG + UART_DR) = (unsigned char)c;
+	USART3->DR = (unsigned char) c;
 	
 	return 0;
 }
@@ -28,7 +18,7 @@ const unsigned char *puts(const unsigned char *_s) {
 int main() {
 	const char *s = "hello, tiny-firmware Pebble!\n";
 	
-	*(unsigned long *)RCC_APB1ENR |= RCC_APB1ENR_UART3;
+	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 	puts("hello, tiny-firmware Pebble, from C code!\n");
 	
 	return 0;
